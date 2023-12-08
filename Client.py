953dfb147f334dcde0e32675de102886bdd89a72
Ithamar <57110382+IthamarBaron@users.noreply.github.com>
2023-12-08 21:18:05 +0200

    Basic file transfer is now possible.

--- Client.py
+++ Client.py
@@ -5,7 +5,6 @@ class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.client_socket = None
 
     def connect_to_server(self):
         try:
@@ -15,16 +14,30 @@ class Client:
         except Exception as e:
             print(f"Connection error: {str(e)}")
 
-    def send_data(self, data, sendall=False):
+    def send_data(self, data, need_encode=True, sendall=False):
         try:
             if sendall:
-                self.client_socket.sendall((str(data)).encode())
-            else:
+                self.client_socket.sendall(data)
+            elif need_encode:
                 self.client_socket.send((str(data)).encode())
+            elif not sendall and not need_encode:
+                self.client_socket.send(data)
         except Exception as e:
             print(f"Error sending data to the server: {e}")
 
+    def send_file_to_server(self, file_name):
+        file = open(file_name, "rb")
+        file_size = os.path.getsize(file_name)
+        file_data = file.read()
+
+        self.send_data(file_name)
+        self.send_data(file_size)
+        self.send_data(file_data, sendall=True)
+        self.send_data(b"[END]", need_encode=False)
+
+        file.close()
 
 if __name__ == "__main__":
     client = Client('LocalHost', 12345)
     client.connect_to_server()
+    client.send_file_to_server("tomerXd.mp4")
--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
-import os
+import warnings
+
 import tqdm
 import socket
 
@@ -7,19 +8,50 @@ class Server:
         self.host = host
         self.port = port
 
-
-
     def start(self):
         # Create a server socket and listen for client connections
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((self.host, self.port))
         server_socket.listen()
-
         print(f"Server is listening for connections")
-        client_socket, addr = server_socket.accept()
-        print(f"New client connected: {client_socket.getpeername()}")
+
+        self.client_socket, _ = server_socket.accept()
+        print(f"New client connected: {self.client_socket.getpeername()}")
+
+    def receive_data(self, bytes_to_read):
+        try:
+            return self.client_socket.recv(bytes_to_read).decode()
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+
+    def receive_file(self):
+        try:
+            file_name = self.receive_data(1024)
+            file_size = int(self.receive_data(1024))
+            print(f"Receiving file: {file_name} of size: {file_size / (1024 ** 2):.2f} MB")
+
+            warnings.simplefilter("ignore", tqdm.TqdmWarning)
+            progress = tqdm.tqdm(unit="MB", unit_scale=True, unit_divisor=1024, total=file_size / (1024 ** 2), position=0)
+
+            file = open(("new_" + file_name), "wb")
+            file_bytes = b""
+            done_receiving = False
+
+            while not done_receiving:
+                part_of_file = self.client_socket.recv(1024)
+                if file_bytes[-5:] == b'[END]':
+                    done_receiving = True
+                else:
+                    file_bytes += part_of_file
+                progress.update(1024 / (1024 ** 2))
+
+            file.write(file_bytes)
+            file.close()
+        except Exception as e:
+            print(f"Error receive file: {e}")
 
 
 if __name__ == "__main__":
     server = Server('0.0.0.0', 12345)
     server.start()
+    server.receive_file()
